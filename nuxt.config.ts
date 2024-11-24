f92811378fc628eaa4bd5b8e82af18ad2dbec323
Anneli Kurut <anneli.kurut@casapay.com>
2024-11-24 22:58:46 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
 	runtimeConfig: {
 		public: {
 			apiUrl: process.env.API_URL || "",
-			baseUrl: process.env.BASE_URL || "/piletilevi-app/",
+			path: process.env.BASE_URL || "/piletilevi-app/",
 		},
 	},
 	routeRules: {
