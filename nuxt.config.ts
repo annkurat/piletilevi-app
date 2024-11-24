3fbdf1ebac7d3cbc92f9df2e10e075d4858e01c0
Anneli Kurut <anneli.kurut@casapay.com>
2024-11-25 00:00:27 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
 	css: ["~/assets/css/main.css"],
 	runtimeConfig: {
 		public: {
-			apiUrl: process.env.API_URL || "",
+			apiUrl: "https://api.intra.piletilevi.ee",
 		},
 	},
 
