6936f77d7e4d386a39485222284a6ee7767c9511
Anneli Kurut <anneli.kurut@casapay.com>
2024-11-24 23:36:31 +0200

    fix

--- server/api/v1/discounts.ts
+++
@@ -1,7 +0,0 @@
-import type { Discount } from "~/types";
-export default defineEventHandler(async () => {
-	const { apiUrl } = useRuntimeConfig().public;
-
-	const response = await $fetch<Discount[]>(`${apiUrl}/v1/discounts`);
-	return response;
-});
