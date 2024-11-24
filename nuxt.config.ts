c983100cf378be404e9dbd9b857df0646351bbaa
Anneli Kurut <anneli.kurut@casapay.com>
2024-11-24 23:18:20 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		baseURL: process.env.BASE_URL || "/piletilevi-app/",
+		buildAssetsDir: "assets",
+	},
 	modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@vueuse/nuxt"],
 	css: ["~/assets/css/main.css"],
 	runtimeConfig: {
